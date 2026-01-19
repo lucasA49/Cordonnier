00e048bf9c51eef4afbabe96ea0d41727eddba73
lucasA49 <lucasaksu@gmail.com>
2026-01-19 19:05:52 +0100

    mise en place de tailwinds css

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className='bg-blue-300'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
